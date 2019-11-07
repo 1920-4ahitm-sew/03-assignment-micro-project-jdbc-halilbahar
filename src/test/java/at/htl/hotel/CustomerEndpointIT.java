b3e5c921a07ea653e36f110719aa022dd5c94222
Halil Bahar <halil.s.bahar@gmail.com>
2019-11-08 00:14:44 +0100

    Create test for customer endpoint

---
+++ src/test/java/at/htl/hotel/CustomerEndpointIT.java
@@ -0,0 +1,32 @@
+package at.htl.hotel;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CustomerEndpointIT {
+    private Client client;
+    private WebTarget webTarget;
+
+    @BeforeEach
+    public void initClient() {
+        this.client = ClientBuilder.newClient();
+        this.webTarget = client.target("http://localhost:8080/vehicle/api/vehicle");
+    }
+
+    @Test
+    public void getAllVehicles() {
+        Response response = this.webTarget.request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(), is(200));
+    }
+
+
+}
